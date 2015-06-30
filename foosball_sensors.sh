2a52ff930eb4d3021ba2f77f55a504613bd7c047
Kipp Bowen <kipp.bowen@axiosengineering.com>
2015-06-30 11:07:07 -0400

    renamed main script and python file

--- foosball_sensors.sh
+++ foosball_sensors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SCRIPT_NAME=/home/pi/gpio/goal_test.py
-PID_FILE=/var/run/goal_test.pid
+SCRIPT_NAME=/home/pi/gpio/foosball_sensors.py
+PID_FILE=/var/run/foosball_sensors.pid
 export PYTHON_PATH=${PYTHON_PATH}:/home/pi/gpio/
 
 case "$1" in
