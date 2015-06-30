d302515bc1718d193d3ddceb5c8b031541e56d0d
Kipp Bowen <kipp.bowen@axiosengineering.com>
2015-06-30 11:02:38 -0400

    added error handling to sending request

--- foosball_utils.py
+++ foosball_utils.py
@@ -16,33 +16,34 @@ class DigitalFoosballSimulator(object):
         headers = {"Content-Type": "application/x-www-form-urlencoded",
                    "User-Agenet": "Arduino/DigitalKicker"
                   }
-        conn = httplib.HTTPConnection(self.ip, self.port)
-        conn.set_debuglevel(1)
-        if tag:
-            params['@tag'] = tag
-            context = self.context + "/events/rfid/%s" % team
-        else:
-            context = self.context + "/events/goals/%s" % team
-        params = urllib.urlencode(params)
+        try:
+            conn = httplib.HTTPConnection(self.ip, self.port)
+            conn.set_debuglevel(1)
+            if tag:
+                params['@tag'] = tag
+                context = self.context + "/events/rfid/%s" % team
+            else:
+                context = self.context + "/events/goals/%s" % team
+            params = urllib.urlencode(params)
 
-        conn.request("POST", context, params, headers)
-        return conn.getresponse()       
+            conn.request("POST", context, params, headers)
+            response = conn.getresponse()
+            return response.status, response.reason
+        except Exception, e:
+            print 'ERROR - exception when sending request\n  params:%s\n  headers:%s'%(params,headers)
+            return 'ERROR', 'exception when sending request, see logs'
 
     def sendVisitorGoal(self):
-        response = self._sendRequest("visitors")
-        print response.status, response.reason
+        print self._sendRequest("visitors")
 
     def sendHomeGoal(self):
-        response = self._sendRequest("home")
-        print response.status, response.reason
+        print self._sendRequest("home")
 
     def addRFIDVistor(self, id):
-        response = self._sendRequest("visitors", id)
-        print response.status, response.reason
+        print self._sendRequest("visitors", id)
 
     def addRFIDHome(self, id):
-        response = self._sendRequest("home", id)
-        print response.status, response.reason
+        print self._sendRequest("home", id)
 
 if __name__ == "__main__":
 
